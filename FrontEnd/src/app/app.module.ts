299e4637de855b545e7f8628fd032c947fbbc6cf
El HATIMI Zakaria <73597675+Zack4DEV@users.noreply.github.com>
2024-07-10 18:23:08 +0100

    Project - . - *

---
+++ FrontEnd/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http'
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { FormsModule } from '@angular/forms';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import {DynamicDialogModule} from 'primeng/dynamicdialog';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeListComponent,
+    CreateEmployeeComponent,
+    UpdateEmployeeComponent,
+  ],
+  imports: [
+    RouterModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ FrontEnd/src/app/employee.ts
@@ -0,0 +1,7 @@
+export class Employee {
+    id!: number; 
+    firstName: string | undefined; 
+    lastName: string | undefined; 
+    emailId: string | undefined; 
+    
+}
